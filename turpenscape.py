2a6e27019dda06cd94402927cfc53866f617bb33
xaviju <xaviju@gmail.com>
2017-01-24 16:14:35 +0100

    Generate Palette

--- turpenscape.py
+++ turpenscape.py
@@ -1,16 +1,12 @@
-import os
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory, after_this_request
-from werkzeug.utils import secure_filename
-from palette_gen import generate_palette
-
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
-UPLOAD_FOLDER = '/static/uploads'
-UPLOAD_FOLDER = '/home/xaviju/Projects/2017/turpenscape/static/uploads'
+from flask import Flask, render_template, request, redirect
+import base64
+from utils.palette_gen import generate_palette
+from utils.slug import slugify
 
 app = Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = 3 * 1024 * 1024
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
 
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -34,39 +30,28 @@ def palette():
 
         if file and allowed_file(file.filename):
 
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
             palette = generate_palette(file)
-            palette_name = request.form['palette_name']
+            palette_name = slugify(request.form['palette_name'])
 
-            template = render_template('palette.gpl', palette=palette, palette_name=palette_name)
+            template = render_template(
+                'palette.gpl',
+                palette=palette,
+                palette_name=palette_name
+            )
 
-            # @after_this_request
-            # def remove_file(response):
-            #      os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file.seek(0)
+            image = base64.b64encode(file.read())
+            image = image.decode("utf-8")
 
-            # filename = secure_filename(file.filename)
-            # palette_file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-
-            # palette_file_edit = open(palette_file, "w")
-            # palette_To_File = raw_input(template)
-            # palette_file_edit.write(palette_To_File)
-            # palette_file_edit.close()
-            #
-            # with open("{}.gpl".format(filename), "r+") as palette_file:
-            #     palette_file.write(template)
-            #     palette_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-            # send_file(palette_file, mimetype="application/octet-stream", as_attachment=True, attachment_filename="{palette_name}.gpl".format(palette_name=palette_name))
-
-            return render_template('palette.html', palette=palette, palette_name=palette_name, template=template, filename=filename)
+            return render_template(
+                'palette.html',
+                palette=palette,
+                palette_name=palette_name,
+                template=template,
+                image=image
+            )
 
     return render_template('create.html')
-
-@app.route('/static/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
     app.config['DEBUG'] = True
