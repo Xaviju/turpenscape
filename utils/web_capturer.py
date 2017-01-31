be4d6308c39766d8e15a6f534fa70e0292fd32c6
David Barragán Merino <david.barragan@kaleidos.net>
2017-01-31 23:27:16 +0100

    Add module to get web screenshots

---
+++ utils/web_capturer.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from pyvirtualdisplay import Display
+from selenium import webdriver
+
+from time import sleep
+import sys
+
+from contextlib import contextmanager
+
+
+@contextmanager
+def create_hidden_display(width, height):
+    display = Display(visible=0, size=(width, height))
+    display.start()
+    yield display
+    display.stop()
+
+@contextmanager
+def create_driver():
+    driver = webdriver.Firefox()
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+
+
+def capture_web(url):
+    with create_hidden_display(1440, 900):
+        with create_driver() as driver:
+            driver.get(url)
+            sleep(5)
+            image_bin = driver.get_screenshot_as_png()
+
+    return image_bin
+
+
+if __name__ == '__main__':
+    # Ex: python web_capturer.py https://taiga.io
+    from io import BytesIO
+    from palette_gen import generate_palette
+
+    image_bin = capture_web(sys.argv[1])
+
+    print(generate_palette(BytesIO(image_bin)))
