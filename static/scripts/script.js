72f79f6fc57dcc94035f781168a8fd7f82659679
xaviju <xaviju@gmail.com>
2017-02-02 17:37:31 +0100

    WIP from URL

---
+++ static/scripts/script.js
@@ -0,0 +1,16 @@
+// const divs = document.querySelectorAll('div');
+//
+// function logText(e) {
+//     console.log(this.classList.value);
+//     // e.stopPropagation(); // stop bubbling!
+//     // console.log(this);
+// }
+// divs.forEach(div => div.addEventListener('click', logText, {
+//     capture: false,
+//     once: true
+// }));
+// button.addEventListener('click', () => {
+//     console.log('Click!!!');
+// }, {
+//     once: true
+// });
